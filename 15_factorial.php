09faaef387fa74206dc2c303239024b82f71835d
Arun B M <146818469+cecad714@users.noreply.github.com>
2023-12-11 09:28:17 +0530

    Update 15_factorial.php
    error correction

--- 15_factorial.php
+++ 15_factorial.php
@@ -3,35 +3,28 @@
   <title>FACTORIAL</title>
  </head>
  <body>
- <form method="POST" action="">
-  Enter a number:
-  <input type="text" name="number">
-  <input type="submit" value="Calcuate Factorial">
- </form>
-<?php
- if($_SERVER["REQUEST_METHOD" == "POST])
-{
-  if(isset($_POST['number']) && is_numeric['number'] )
-  {
-    $number = $_POST['number'];
-    $factorial
-  if()
-  {
-   if($number%2==0)
-   {
-    echo "{$number} is even.";
-   }
-   else
-   {
-    echo "{$number} is odd.";
-   }
+  <form method="POST" action="">
+   Enter a number:
+   <input type="text" name="number">
+   <input type="submit" value="Calculate Factorial">
+  </form>
+
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['number']) && is_numeric($_POST['number'])) {
+      $number = $_POST['number'];
+      $factorial = 1;
+
+      // Calculate factorial
+      for ($i = 1; $i <= $number; $i++) {
+        $factorial *= $i;
+      }
+
+      echo "Factorial of {$number} is {$factorial}.";
+    } else {
+      echo "Please enter a valid number!!";
+    }
   }
-  else
-  {
-   echo "Please enter a valid  number!!";
-  }
- }
-}
-?>
-   </body>
+  ?>
+ </body>
 </html>
